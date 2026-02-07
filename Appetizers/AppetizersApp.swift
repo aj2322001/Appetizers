1ee75bc132543489361f6406591ad83fa4afa346
Archit Joshi <archit.joshi@anarock.com>
2026-02-07 15:48:56 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Archit  Joshi on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
